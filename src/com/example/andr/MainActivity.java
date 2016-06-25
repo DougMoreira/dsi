a31165825737a8184bdfa89a4cc2d371918d4974
Douglas Moreira <douglasa321@gmail.com>
2016-06-25 18:16:08 -0300

    Implementação de Socket

--- src/com/example/andr/MainActivity.java
+++ src/com/example/andr/MainActivity.java
@@ -10,9 +10,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
 public class MainActivity extends Activity {
-	
 	private EditText nomeEditText;
 	private TextView saudacaoTextView;
 	private String saudacao;
@@ -20,70 +18,54 @@ public class MainActivity extends Activity {
 	private EditText portaServidor;
 	private SocketTask st;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        
-        
-        this.nomeEditText = (EditText) findViewById(R.id.enderecoServidor);
-        this.saudacaoTextView =
-        (TextView) findViewById(R.id.saudacaoTextView);
-        this.saudacao = getResources().getString(R.string.saudacao);
-        
-        
-        
-        
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		this.nomeEditText = (EditText) findViewById(R.id.enderecoServidor);
+		this.saudacaoTextView =
+				(TextView) findViewById(R.id.saudacaoTextView);
+		this.saudacao = getResources().getString(R.string.saudacao);
 
-    }
-    
-    public void surpreenderUsuario(View v) {
-    	Intent intent = new Intent(SaudacaoActivity.ACAO_EXIBIR_SAUDACAO);
-    	
-    	intent.addCategory(SaudacaoActivity.CATEGORIA_SAUDACAO);
-    	String texto = nomeEditText.getText().toString();
-    	intent.putExtra(SaudacaoActivity.EXTRA_NOME_USUARIO, texto);
-    	startActivity(intent);
-    	}
-    
-    public void login(View v){
-    	 this.enderecoServidor = (EditText) findViewById(R.id.enderecoServidor);
-    	 this.portaServidor = (EditText) findViewById(R.id.portaServidor);
-    	 
-    	 String servidor = enderecoServidor.getText().toString();
-    	 int porta = Integer.parseInt(portaServidor.getText().toString());
-    	 
-    	 st = new SocketTask(servidor, porta, 10000);
-    	 Toast.makeText(MainActivity.this, ":" + st.doInBackground().booleanValue(), Toast.LENGTH_LONG).show();
-    	 st.execute();
-    	
-    	
-    }
-    
-/* 1 */    
-//    public void surpreenderUsuario(View v) {
-//    	Editable texto = this.nomeEditText.getText();
-//    	String msg = saudacao + " " + texto.toString();
-//    	this.saudacaoTextView.setText(msg);
-//    	}
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	public void surpreenderUsuario(View v) {
+		Intent intent = new Intent(SaudacaoActivity.ACAO_EXIBIR_SAUDACAO);
+		intent.addCategory(SaudacaoActivity.CATEGORIA_SAUDACAO);
+		String texto = nomeEditText.getText().toString();
+		intent.putExtra(SaudacaoActivity.EXTRA_NOME_USUARIO, texto);
+		startActivity(intent);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	public void login(View v) throws InterruptedException{
+		this.enderecoServidor = (EditText) findViewById(R.id.enderecoServidor);
+		this.portaServidor = (EditText) findViewById(R.id.portaServidor);
+
+		String servidor = enderecoServidor.getText().toString();
+		int porta = Integer.parseInt(portaServidor.getText().toString());
+		Toast.makeText(MainActivity.this, "IP: " + servidor + " | Porta: " + porta , Toast.LENGTH_LONG).show();
+
+		st = new SocketTask(servidor, porta, 10000);
+		st.execute();
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
